3d123eddd6d0f9a3767593665c5e48ca8a57f520
Duduzzn22 <dudualmeidazz9286@gmail.com>
2026-09-04 22:25:49 -0300

    feat: initialize project hydrator before viewer

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -6,12 +6,16 @@ import { initReveal } from "./reveal.js";
 import { initSectionNav } from "./section-nav.js";
 import { initFooterYear } from "./footer-year.js";
 import { initProjectDialog } from "./project-dialog.js";
+import { initProjectHydrator } from "./project-hydrator.js";
 import { initSignalFrame } from "./signal-frame.js";
 import { initProjectForm } from "./project-form.js";
 import { initPointerEffects } from "./pointer-effects.js";
 
 document.documentElement.classList.add("js");
 
+// Publica cases que ainda sao placeholders no HTML antes que o Project Viewer
+// leia os slots e conecte os listeners.
+initProjectHydrator();
 initScrollProgress();
 initMenu();
 initReveal();
